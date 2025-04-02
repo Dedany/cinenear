8394e50c3aaf1026ffc43d9ee3dbcea931381b31
Daniel Pérez Álvarez <danylerez@gmail.com>
2025-04-02 13:31:03 +0200

    feat: Implement DetailViewModel
    - Create `DetailViewModel` to fetch movie data by ID.
    - Set loading state in `DetailViewModel`.

---
+++ app/src/main/java/com/dedany/cinenear/ui/screens/detail/DetailViewModel.kt
@@ -0,0 +1,33 @@
+package com.dedany.cinenear.ui.screens.detail
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dedany.cinenear.data.Movie
+import com.dedany.cinenear.data.MoviesRepository
+import kotlinx.coroutines.launch
+
+
+class DetailViewModel(private val id: Int) : ViewModel() {
+
+    private val repository = MoviesRepository()
+
+    var state by mutableStateOf(UiState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            state = UiState(loading = true)
+            state = UiState(loading = false, movie = repository.findMovieById(id))
+        }
+    }
+
+
+    data class UiState(
+        val loading: Boolean = false,
+        val movie: Movie? = null,
+    )
+
+}
