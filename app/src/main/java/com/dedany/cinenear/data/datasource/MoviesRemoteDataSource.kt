2fdd46b2c5414363d60372be6d9581e83f79914e
Daniel Pérez Álvarez <danylerez@gmail.com>
2025-04-09 11:53:18 +0200

    Add moviesRemoteDataSource and changes dependency

---
+++ app/src/main/java/com/dedany/cinenear/data/datasource/MoviesRemoteDataSource.kt
@@ -0,0 +1,37 @@
+package com.dedany.cinenear.data.datasource
+
+import com.dedany.cinenear.data.Movie
+import com.dedany.cinenear.data.MoviesClient
+import com.dedany.cinenear.data.RemoteMovie
+
+class MoviesRemoteDataSource {
+
+    suspend fun fetchPopularMovies(region: String): List<Movie> =
+        MoviesClient
+            .instance
+            .fetchPopularMovies(region)
+            .results
+            .map { it.toDomainModel() }
+
+    suspend fun findMovieById(id: Int): Movie =
+        MoviesClient
+            .instance
+            .fetchMovieById(id)
+            .toDomainModel()
+}
+}
+
+
+private fun RemoteMovie.toDomainModel(): Movie =
+    Movie(
+        id = id,
+        title = title,
+        overview = overview,
+        releaseDate = releaseDate,
+        poster = "https://image.tmdb.org/t/p/w185/$posterPath",
+        backdrop = if (backdropPath != null) "https://image.tmdb.org/t/p/w780/$backdropPath" else null,
+        originalTitle = originalTitle,
+        originalLanguage = originalLanguage,
+        popularity = String.format("%.1f", popularity.toFloat()),
+        voteAverage = String.format("%.1f", voteAverage.toFloat()),
+    )
